729c8dc406bffa9405e7a3e924fac53be8a55f32
dev-Alencar <lucalencar.dev@gmail.com>
2021-10-25 18:09:58 -0300

    nlw/2021/ aula 4

--- script.js
+++ script.js
@@ -11,13 +11,21 @@ function changeSocialMediaLinks() {
         const social = li.getAttribute('class')
         li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
 
-        // if(linksSocialMedia[social] = linkedin) {
-        //     const social = li.getAttribute('class')
-        //     li.children[0].href = `https://${social}.com/in/${linksSocialMedia[social]}`
-        // }
-
-        // alert(li.children[0].href)
     } 
-    
 }
+
 changeSocialMediaLinks() 
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+    .then(r => r.json())
+    .then(data => { 
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href= data.html_url
+     })
+}
+
+getGitHubProfileInfos()
